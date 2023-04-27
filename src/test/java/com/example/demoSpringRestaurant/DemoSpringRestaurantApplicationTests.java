d068c69a5ba20bd129d480b2a76eee1e50635c0e
Nagy László Levente <nagylacika2002@gmail.com>
2023-04-27 12:16:48 +0200

    Initial commit

---
+++ src/test/java/com/example/demoSpringRestaurant/DemoSpringRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSpringRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
