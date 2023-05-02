a35ef7fdfb65d38a848549af82559a68c439b8c9
Nagy László Levente <nagylacika2002@gmail.com>
2023-05-02 20:48:10 +0200

    Created OrderEntity class

---
+++ src/main/java/com/example/demoSpringRestaurant/persistance/entity/OrderEntity.java
@@ -0,0 +1,88 @@
+package com.example.demoSpringRestaurant.persistance.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class OrderEntity {
+    @Id
+    @SequenceGenerator(name = "order",sequenceName = "order_sequence",initialValue = 1)
+    @GeneratedValue(generator = "order_sequence",strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    enum DrinkType {
+        COLA, WATER, JUICE, LEMONADE, TEA
+    }
+
+    public enum MealType {
+        RICEANDFISH, FISHANDCHIPS, CHICKENANDRICE, CHICKENANDFISH
+    }
+
+    private MealType Meal;
+    private DrinkType Drink;
+    private double price = getFoodPrice(Meal) + getDrinkPrice(Drink);
+
+    private double getDrinkPrice(DrinkType drinkType) {
+        return switch (drinkType) {
+            case COLA -> 3.1;
+            case WATER -> 1.0;
+            case JUICE,LEMONADE -> 2.6;
+            case TEA -> 1.1;
+        };
+    }
+
+    private double getFoodPrice(MealType mealType) {
+        return switch (mealType) {
+            case RICEANDFISH -> 10.1;
+            case FISHANDCHIPS -> 12.1;
+            case CHICKENANDFISH -> 20.1;
+            case CHICKENANDRICE -> 15.1;
+        };
+    }
+
+    public OrderEntity(Long id, MealType meal, DrinkType drink, double price) {
+        this.id = id;
+        Meal = meal;
+        Drink = drink;
+        this.price = price;
+    }
+
+    public OrderEntity(MealType meal, DrinkType drink, double price) {
+        Meal = meal;
+        Drink = drink;
+        this.price = price;
+    }
+    public OrderEntity() {
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public MealType getMeal() {
+        return Meal;
+    }
+
+    public void setMeal(MealType meal) {
+        Meal = meal;
+    }
+
+    public DrinkType getDrink() {
+        return Drink;
+    }
+
+    public void setDrink(DrinkType drink) {
+        Drink = drink;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    /*public void setPrice(double price) {
+        this.price = price;
+    }*/
+}
